fcae495457474d7d62995fd7720cb4048e98563e
fgndwikasetiawan <fgn.dwikasetiawan@gmail.com>
2015-03-18 22:40:07 +0700

    fix rpc

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import xmlrpclib as xrl
 import os
 import threading
+import time
 
-
-def kirim_gambar(daftarFile, server, jumlah):
+def kirim_gambar(daftarFile, server, jumlah, nama):
     daftarGambar = []
     n = 0
     while (n + jumlah < len(daftarFile)):
@@ -20,7 +20,7 @@ def kirim_gambar(daftarFile, server, jumlah):
             i += 1
         daftarGambar[:] = []
         n += jumlah
-        print str(n) + '/' + str(len(daftarFile))
+        print nama + ": " + str(n) + '/' + str(len(daftarFile))
 
     for i in range(n, len(daftarFile)):
         f = open(daftarFile[i], 'rb')
@@ -33,18 +33,26 @@ def kirim_gambar(daftarFile, server, jumlah):
         f.write(grayscale.data)
         f.close()
         i += 1
-    print('selesai')
+    print nama + ' selesai'
+
 
 dir = raw_input("Direktori gambar: ")
-alamatServer = raw_input("Alamat server: ")
+alamat1 = raw_input("Alamat server #1: ")
+alamat2 = raw_input("Alamat server #2: ")
 os.chdir(dir)
-server = xrl.ServerProxy(alamatServer)
-print "Berhasil konek"
+server1 = xrl.ServerProxy(alamat1)
+server2 = xrl.ServerProxy(alamat2)
 jumlahBatch = int(raw_input("Jumlah gambar per request: "))
 files = os.listdir(os.getcwd())
 jumlahFile = len(files)
 
-thread1 = threading.Thread(target=kirim_gambar, args=(files[:], server, jumlahBatch))
+thread1 = threading.Thread(target=kirim_gambar, args=(files[0:jumlahFile/2], server1, jumlahBatch, 'Thread 1'))
+thread2 = threading.Thread(target=kirim_gambar, args=(files[jumlahFile/2:jumlahFile], server2, jumlahBatch, 'Thread 2'))
+start = time.time()
 thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+print 'SELESAI yeee, waktu: ' + str(time.time() - start)
 
 
